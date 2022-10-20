d74eef94527845ffa1951c97910d606dc372fba8
周建峰 <zhoujianfeng@yixia.com>
2022-10-20 10:05:04 +0800

    first commit

---
+++ magicindicator/src/main/java/net/lucode/hackware/magicindicator/ViewPager2Helper.java
@@ -0,0 +1,28 @@
+package net.lucode.hackware.magicindicator;
+
+import androidx.viewpager2.widget.ViewPager2;
+
+public class ViewPager2Helper {
+
+    public static void bind(final MagicIndicator magicIndicator, ViewPager2 viewPager) {
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+                magicIndicator.onPageScrolled(position, positionOffset, positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                magicIndicator.onPageSelected(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                super.onPageScrollStateChanged(state);
+                magicIndicator.onPageScrollStateChanged(state);
+            }
+        });
+    }
+}
